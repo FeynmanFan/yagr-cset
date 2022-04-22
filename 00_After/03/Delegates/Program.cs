70cd41b3eb47030c139cdb5add492082cfb4ced5
Chris B. Behrens <cbehrens@gmail.com>
2022-04-22 12:03:24 -0500

    Another shot

--- 00_After/03/Delegates/Program.cs
+++ 00_After/03/Delegates/Program.cs
@@ -11,15 +11,10 @@ namespace Delegates
             var constantExpression = Expression.Constant(12);
             var greaterThan = Expression.GreaterThan(xExpression, constantExpression);
 
-            var constant4Expression = Expression.Constant(4);
-            var lessThan = Expression.LessThan(xExpression, constant4Expression);
-
-            var or = Expression.Or(greaterThan, lessThan);
-
-            var expr = Expression.Lambda<Func<int, bool>>(or, false, new List<ParameterExpression> { xExpression, });
+            var expr = Expression.Lambda<Func<int, bool>>(greaterThan, false, new List<ParameterExpression> { xExpression, });
             var func = expr.Compile();
 
-            Console.WriteLine(func(2));
+            Console.WriteLine(func(11));
         }
     }
 }
